307fcbb01ad3e05242ac52217940878053d93c90
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-01-04 10:07:23 -0500

    do you speak english code wars

---
+++ 8kyu/do-you-speak-english.js
@@ -0,0 +1,15 @@
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+function spEng(sentence) {
+  let lowercaseSentence = sentence.toLowerCase();
+  let lowercasedWord = "english";
+  return lowercaseSentence.includes(lowercasedWord);
+}
+
+console.log(spEng("englshdfjsidklfjsdklfjs"));
