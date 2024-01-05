e34fc69bd77694abd34c86195cbf78bb330549ca
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-01-05 09:40:31 -0500

    how many stairs codewars

---
+++ 8kyu/how-many-stairs.js
@@ -0,0 +1,28 @@
+// Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some days he climbs more stairs than others depending on the number of students he must train in the morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
+
+// The sum of all the stairs logged in a year will be used for estimating the number he might climb in 20.
+
+// 20_year_estimate = one_year_total * 20
+
+// You will receive the following data structure representing the stairs Suzuki logged in a year. You will have all data for the entire year so regardless of how it is logged the problem should be simple to solve.
+
+function stairsIn20(s) {
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s[i].length; j++) result += s[i][j];
+  }
+  return result * 20;
+}
+
+const stairs = [
+  [
+    6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274,
+    7148, 6152, 5940, 8040, 9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478,
+    6063, 5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097, 6249, 8381,
+    5936, 8496, 6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032,
+    9871, 5990, 6309, 7825,
+  ],
+];
+
+const estimateFor20Years = stairsIn20(stairs);
+console.log(estimateFor20Years);
