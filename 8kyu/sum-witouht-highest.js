6064a2e8a08c59315c2b5c475234edc125a0e43d
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-01-28 08:51:55 -0500

    codewars challenge

--- 8kyu/sum-witouht-highest.js
+++ 8kyu/sum-witouht-highest.js
@@ -8,4 +8,20 @@
 // { 6, 2, 1, 8, 10 } => 16
 // { 1, 1, 11, 2, 3 } => 6
 
-function sumArray(array) {}
+function sumArray(array) {
+  if (!array || array.length <= 2) {
+    return 0;
+  }
+
+  let min = Math.min(...array);
+  let max = Math.max(...array);
+
+  let sum = array.reduce((acc, num) => acc + num, 0);
+
+  sum = sum - min - max;
+
+  return sum;
+}
+
+console.log(sumArray([6, 2, 1, 8, 10]));
+console.log(sumArray([1, 1, 11, 2, 3]));
