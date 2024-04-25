9a3f880780756a73f0f36300a8647de5e7a1040f
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-04-25 07:50:52 -0400

    even or odd codewars

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,15 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// get the number
+// if divisible by 2 return even
+// if not odd
+// loop through
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log(evenOrOdd(4, 6, 8));
