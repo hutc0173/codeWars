37ca2db7833abb9ffba80a89646f639475ca73fc
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-06-22 10:50:08 -0400

    to upper case code wars

---
+++ 8kyu/make-upper.js
@@ -0,0 +1,8 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+const result = makeUpperCase("dog");
+console.log(result);
