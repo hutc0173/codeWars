812892b47ef3d711ff9adbbf6c849cd815238417
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-07-06 08:50:40 -0400

    find the smallest

---
+++ 8kyu/find-the-smallest.js
@@ -0,0 +1,21 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+function findSmallestInt(arr) {
+  let smallest = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+    }
+  }
+
+  return smallest;
+}
+console.log(findSmallestInt([34, 15, 88, 2]));
+console.log(findSmallestInt([34, -345, -1, 100]));
