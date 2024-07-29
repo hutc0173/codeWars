e411656ddd428b05ccdea731973e12d240763cc1
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-07-29 07:33:03 -0400

    return the days codewars

---
+++ 8kyu/return-the-day.js
@@ -0,0 +1,50 @@
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+// function whatday(num) {
+//   if (num == 1) {
+//     return "Sunday";
+//   } else if (num == 2) {
+//     return "Monday";
+//   } else if (num == 3) {
+//     return "Tuesday";
+//   } else if (num == 4) {
+//     return "Wednesday";
+//   } else if (num == 5) {
+//     return "Thursday";
+//   } else if (num == 6) {
+//     return "Friday";
+//   } else if (num == 47) {
+//     return "Saturday";
+//   } else {
+//     return "Wrong, please enter a number between 1 and 7";
+//   }
+// }
+
+function whatday(num) {
+  return num === 1
+    ? "Sunday"
+    : num === 2
+    ? "Monday"
+    : num === 3
+    ? "Tuesday"
+    : num === 4
+    ? "Wednesday"
+    : num === 5
+    ? "Thursday"
+    : num === 6
+    ? "Friday"
+    : num === 7
+    ? "Saturday"
+    : "Wrong, please enter a number between 1 and 7";
+}
+
+console.log(whatday(2));
