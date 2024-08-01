e8f9e350b456e4ac6e4ec4b54f626dae8cdcb82b
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-08-01 08:02:17 -0400

    are you playing banjo code wars completed

---
+++ 8kyu/are-you-playing-banjo.js
@@ -0,0 +1,19 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+
+function areYouPlayingBanjo(name) {
+  let firstChar = name.charAt(0).toLowerCase();
+
+  if (firstChar === "r") {
+    return name + " " + "plays banjo";
+  } else {
+    return name + " " + "does not play banjo";
+  }
+}
+
+console.log(areYouPlayingBanjo("Rave"));
