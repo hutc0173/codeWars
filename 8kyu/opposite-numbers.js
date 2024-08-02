6de10d883aa04d82b48a67827c8433012c22389f
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-08-02 07:38:37 -0400

    opposite number code wars

---
+++ 8kyu/opposite-numbers.js
@@ -0,0 +1,13 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return -number;
+}
+
+console.log(opposite(-25));
