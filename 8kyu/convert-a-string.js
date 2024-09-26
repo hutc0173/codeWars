9a628cad552b215c99503f8e562fb02cd660e47b
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-09-26 08:57:42 -0400

    string to array codewars

---
+++ 8kyu/convert-a-string.js
@@ -0,0 +1,7 @@
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+console.log(stringToArray("Hello world! How are you?"));
